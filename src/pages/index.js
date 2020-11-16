c67603221bd71b75f308e77b46b354de3e4513a0
s34rching <ivan.katsenya@gmail.com>
2020-11-16 14:54:32 +0300

    Add styles to posts preview

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,17 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
+const BlogLink = styled(Link)`
+  text-decoration: none,
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+`
 
 const IndexPage = ({ data }) => (
   <Layout>
@@ -13,7 +22,11 @@ const IndexPage = ({ data }) => (
       {
         data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
-            <span>{node.frontmatter.title} - {node.frontmatter.date}</span>
+            <BlogLink to={node.fields.slug}>
+              <BlogTitle>
+                {node.frontmatter.title} - {node.frontmatter.date}
+              </BlogTitle>
+            </BlogLink>
             <p>{node.excerpt}</p>
           </div>
         ))
@@ -27,7 +40,7 @@ const IndexPage = ({ data }) => (
 export default IndexPage;
 export const query = graphql`
   query {
-    allMarkdownRemark {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
     totalCount
       edges {
         node {
@@ -36,6 +49,9 @@ export const query = graphql`
             description
             title
             date
+          }
+          fields {
+            slug
           }
           excerpt
         }
