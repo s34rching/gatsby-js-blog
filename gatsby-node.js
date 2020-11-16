ae942a78d4493213a4c1d43993ae6ba7f4c44617
s34rching <ivan.katsenya@gmail.com>
2020-11-16 14:40:10 +0300

    Setup pages creation

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,8 @@
-const { createFilePath } = require(`gatsby-source-filesystem`);
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions;
+  const { createNodeField } = actions
 
   if (node.internal.type === `MarkdownRemark`) {
     const slug = createFilePath({ node, getNode })
@@ -12,4 +13,32 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
       value: slug
     })
   }
+}
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  return graphql(`
+  {
+    allMarkdownRemark {
+      edges {
+        node {
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+  `).then(result => {
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve(`./src/templates/blog-post.js`),
+        context: {
+          slug: node.fields.slug,
+        }
+      })
+    })
+  })
 }
